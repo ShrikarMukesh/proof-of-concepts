5f92ba83ee0dc8b2630f02dc73cd684fbcaf58d5
ShrikarMukesh <mukesh.shrikar7@gmail.com>
2020-08-17 16:13:57 +0530

    First Commit

---
+++ info-rabbitmq/src/main/java/com/basic/Main.java
@@ -0,0 +1,25 @@
+package com.basic;
+
+import java.util.HashMap;
+
+public class Main {
+	public Main() throws Exception{
+		
+		QueueConsumer consumer = new QueueConsumer("queue");
+		Thread consumerThread = new Thread(consumer);
+		consumerThread.start();
+		
+		Producer producer = new Producer("queue");
+		
+		for (int i = 0; i < 100000; i++) {
+			HashMap<String, Integer> message = new HashMap<String, Integer>();
+			message.put("message number", i);
+			producer.sendMessage(message);
+			System.out.println("Message Number "+ i +" sent.");
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+	  new Main();
+	}
+}
